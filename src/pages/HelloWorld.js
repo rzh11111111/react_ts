bde477bb39e8d5b180681dd4e1413bc4f1f67e63
renzonghao <294346639@qq.com>
2022-03-23 17:30:51 +0800

    feat:传递参数

--- src/pages/HelloWorld.js
+++ src/pages/HelloWorld.js
@@ -1,11 +1,12 @@
 import { useState } from 'react';
-export default function HelloWorld() {
+export default function HelloWorld(props) {
   const [styleData] = useState({ color: 'red', fontSize: '16px' });
   const [isHead] = useState(true);
   const [className] = useState('title');
+  const { title = 'hello world' } = props;
   return (
     <div className={`${className} ${isHead ? 'head' : ''}`} style={styleData}>
-      hello world
+      hello world{title}
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
 import styles from './index.less';
+import { useState } from 'react';
 import HelloWorld from './HelloWorld.js';
 export default function IndexPage() {
+  const [styleData] = useState({ color: 'red', fontSize: '16px' });
+  const [isHead] = useState(true);
+  const [className] = useState('title');
   return (
     <div>
+      {/* 除了传递字符不用{}，其他都要 */}
       <h1 className={styles.title}>Page index</h1>
-      <HelloWorld />
+      <HelloWorld
+        styleData={styleData}
+        isHead={isHead}
+        className={className}
+        title="hello vue"
+        num={1}
+      />
     </div>
   );
 }
