c0d8c453c142e820c7a27b9e8e5809fcdac2f391
renzonghao <294346639@qq.com>
2022-03-24 17:03:20 +0800

    feat;动态修改属性

--- src/pages/HelloWorld.js
+++ src/pages/HelloWorld.js
@@ -1,20 +1,19 @@
 import { useState } from 'react';
-export default function HelloWorld(props) {
-  const [styleData] = useState({ color: 'red', fontSize: '16px' });
-  const [isHead] = useState(true);
-  const [className] = useState('title');
-  const { title = 'hello world' } = props;
-  const { onClick } = props;
+export default function HelloWorld() {
+  const [title, setTitle] = useState('hello world');
+  const [className, setClassName] = useState('title');
+  //   在React中称内部数据为state，使用useState(param)定义一个state时，可以通过参数param设置state的默认值，
+  // 其返回一个数组，数组的第一个值是state，数组的第二个值是改变state的函数，可以调用该函数来改变state。
+  // 另外用useState定义的数据是响应式的，若页面有使用该数据，该数据改变后页面会重新渲染。
+
   const handleClick = () => {
-    onClick();
+    setTitle('hello react');
+    setClassName('title active');
   };
   return (
-    <div
-      className={`${className} ${isHead ? 'head' : ''}`}
-      style={styleData}
-      onClick={handleClick}
-    >
-      hello world{title}
+    <div className={className} onClick={handleClick}>
+      {className}
+      {title}
     </div>
   );
 }
