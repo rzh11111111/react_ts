d5faa07a3af9f6248addb99f308ade0b8acbd651
renzonghao <294346639@qq.com>
2022-03-25 17:06:07 +0800

    feat;子组件通知父组件

--- src/pages/HelloWorld.js
+++ src/pages/HelloWorld.js
@@ -1,19 +1,12 @@
-import { useState } from 'react';
-export default function HelloWorld() {
-  const [title, setTitle] = useState('hello world');
-  const [className, setClassName] = useState('title');
-  //   在React中称内部数据为state，使用useState(param)定义一个state时，可以通过参数param设置state的默认值，
-  // 其返回一个数组，数组的第一个值是state，数组的第二个值是改变state的函数，可以调用该函数来改变state。
-  // 另外用useState定义的数据是响应式的，若页面有使用该数据，该数据改变后页面会重新渲染。
-
-  const handleClick = () => {
-    setTitle('hello react');
-    setClassName('title active');
+export default function HelloWorld(props) {
+  const { title = 'hello world', changeTitle } = props;
+  const handleChangeTitle = () => {
+    changeTitle('hello React');
   };
   return (
-    <div className={className} onClick={handleClick}>
-      {className}
+    <div>
       {title}
+      <button onClick={handleChangeTitle}>改变标题</button>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,10 @@
-import styles from './index.less';
 import { useState } from 'react';
 import HelloWorld from './HelloWorld.js';
-export default function IndexPage() {
-  const [styleData] = useState({ color: 'red', fontSize: '16px' });
-  const [isHead] = useState(true);
-  const [className] = useState('title');
 
-  const handleClick = () => {
-    console.log('点击事件');
+export default function Index() {
+  const [info, setInfo] = useState('hello world');
+  const handleChangeTitle = (data: any) => {
+    setInfo(data);
   };
-  return (
-    <div>
-      {/* 除了传递字符不用{}，其他都要 */}
-      <h1 className={styles.title}>Page index</h1>
-      <HelloWorld
-        styleData={styleData}
-        isHead={isHead}
-        className={className}
-        title="hello vue"
-        num={1}
-        onClick={() => {
-          handleClick();
-        }}
-      />
-    </div>
-  );
+  return <HelloWorld title={info} changeTitle={handleChangeTitle} />;
 }
